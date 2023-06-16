cb781a6802e105ffac9322d7a417bd2e7dc8bfca
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-16 16:41:17 -0700

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 a = n%10;
 if (a == 0)
 {
-ptintf("Last digit of %d is 0 and is 0",n);
+printf("Last digit of %d is 0 and is 0",n);
 }
 if (a > 5)
 {
@@ -26,5 +26,6 @@ printf("Last digit of %d is %d and is greater than 5", n, a);
 if (a < 6 && a != 0)
 {
 printf("Last digit of %d is %d and is less than 6", n, a);
+}
 return (0);
 }
