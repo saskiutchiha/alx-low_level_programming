f9c83c11cbbcbee794bd167e39ac1ab7e17bcb66
saskiutchiha <boulidamabdellah8@gmail.com>
2023-06-15 18:30:08 -0700

    ll

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+char a[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *p;
+for (p=&a[0] ;*p != '\0';p++)
+{
+putchar(*p);
+}
+    return (1);
+}
