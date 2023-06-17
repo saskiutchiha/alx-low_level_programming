47866fe25676db24d755bcf065dea5d600839a7c
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-16 18:28:56 -0700

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+int i, j;
+{
+for (i = 0; i <= 9; i++)
+{
+for (i = 0; j <= 9; j++)
+{
+if (j > i)
+{
+printf("%d%d,\t", i, j);
+}
+}
+}
+return (0);
+}
