19a8b38e693f1e5583ed490fa1fa0755e6cc45a9
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-16 17:52:56 -0700

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = o; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
