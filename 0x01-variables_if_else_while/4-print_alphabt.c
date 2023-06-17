91bc6e41fb1ad795190d2d88f5798aec1bade73b
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-16 17:44:13 -0700

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a = 'a', b;
+int i;
+for (i = 0; i <= 25; i++)
+{
+b = a + i;
+if (b != 'e' && b != 'q')
+{
+putchar(b);
+}
+}
+puchar('\n')
+return (0);
+}
