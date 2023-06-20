6d9bf665c912e2845f41642f0492cb0908ca8590
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-20 08:37:00 -0700

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,3 @@
 #include <stdio.h>
-void print_alphabet(void)
-{
-int i;
-for (i = 97; i <= 122; i++)
-{
-putchar(i);
-}
-putchar('\n');
-}
-void print_alphabet_x10(void)
-{
-int j, i;
-for (j = 0; j < 10; j++)
-{
-for (i = 97; i <= 122; i++)
-{
-putchar(i);
-}
-putchar('\n');
-}
-}
+void print_alphabet(void);
+void print_alphabet_x10(void);
