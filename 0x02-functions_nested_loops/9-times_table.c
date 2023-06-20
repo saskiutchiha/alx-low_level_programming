40d5adf01ae0a62013b8f9de696eefb8d44f991c
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-20 15:32:12 -0700

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ for (i = 0; i <= 9;i++)
 for (j = 0;j <= 8; j++)
 {
 printf("%d", s);
-if (s < 10)
+if ((s + i) <= 10)
 {
 printf(",  ");
 }
@@ -18,7 +18,6 @@ printf(", ");
 s = s + i;
 }
 printf("%d", s);
-putchar('$');
 s = 0;
 putchar('\n');
 }
