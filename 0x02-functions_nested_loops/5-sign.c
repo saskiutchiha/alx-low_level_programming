73260e7c2da9028e428824ac679e4788201360a2
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-20 09:57:59 -0700

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int print_sign(int n)
+{
+if (n > 0)
+{
+printf("+");
+return (1);
+}
+if (n == 0)
+{
+printf("0");
+return (0);
+}
+if (n < 0)
+{
+printf("-");
+return (-1);
+}
+}
