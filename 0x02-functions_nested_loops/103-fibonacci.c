0a94a4fd680af260be0954ee96ad9eeb363d10e8
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-20 19:31:33 -0700

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+long  int a = 1, b = 2, s = 0, i, sum;
+sum = a + b;
+while ( s < 4000000)
+{
+sum = sum + s;
+s = a + b;
+a = b;
+b = s;
+}
+printf("%ld",sum);
+putchar('\n');
+return (0);
+}
