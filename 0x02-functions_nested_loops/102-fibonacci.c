fa1ccd5d8b1498337f773b93fb5a0ddb749faf35
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-20 19:08:52 -0700

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+int a = 1; b= 2 ,s, i;
+printf("%d", a);
+printf(", ");
+printf("%d", b);
+for (i=1;i <= 48;i++)
+{
+printf(", ");
+s = a + b;
+printf("%d", s);
+a = b;
+b = s;
+return (0);
+}
