c72ddd14960178f0c737663d4035012326724dc3
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-20 17:34:34 -0700

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,10 @@
 # include <stdio.h>
-
+/**
+*  add- Entry point
+* @a: Number to pass to add function
+* @b: Number to pass to add function
+* Return: integer (Success)
+*/
 int add(int a, int b)
 {
 return (a + b);
