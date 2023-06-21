d67de9e55d929399d9840ab66a617731c1d3442e
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-20 18:44:38 -0700

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int i;
+int i, s =0;
 for (i = 1; i < 1024; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)
