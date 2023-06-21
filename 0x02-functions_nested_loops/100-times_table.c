792199c9ef1f314d5817d2ab9a149763734f4f25
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-20 18:14:02 -0700

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,12 +16,16 @@ for (j = 0; j <= n -1; j++)
 printf("%d", s);
 if ((s + i) < 10)
 {
+printf(",   ");
+}
+if ((s + i) > 10)
+{
 printf(",  ");
 }
-else
+if ((s + i) > 100)
 {
 printf(", ");
-}
+}  
 s = s + i;
 }
 printf("%d", s);
