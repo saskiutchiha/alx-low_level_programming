edabdd841289051a34d4f1576ab99607b99ce2f4
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-20 19:56:14 -0700

    Create 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+long  int a = 1, b = 2, s, i;
+printf("%ld", a);
+printf(", ");
+printf("%ld", b);
+for (i = 1 ; i <= 98 ; i++)
+{
+printf(", ");
+s = a + b;
+printf("%ld", s);
+a = b;
+b = s;
+}
+putchar('\n');
+return (0);
+}
