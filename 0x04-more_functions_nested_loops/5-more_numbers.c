e2e9eff007109fb1c883a79de5e691086fd448db
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-22 11:00:58 -0700

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+void more_numbers(void)
+{
+i, a, b, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 15; j++)
+{
+a = i / 10;
+b = i % 10;
+if (i > 9)
+{
+_putchar(a + 48);
+}
+_putchar(b + 48);
+}
+_putchar('\n');
+}
+}
