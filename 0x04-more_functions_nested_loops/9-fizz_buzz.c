958453bbf8e15847a7d7bbfc5ef52593587f23b6
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-23 15:57:52 -0700

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 int i;
-for (i = 1; i <= 100; i++)
+for (i = 1; i <= 99; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
@@ -27,6 +27,7 @@ printf("%d", i);
 }
 printf(" ");
 }
+printf("Buzz");
 printf("\n");
 return (0);
 }
