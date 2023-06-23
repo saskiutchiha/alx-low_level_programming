a361691926e4a82ce5e5c4057b22efe18dfa353e
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-22 17:45:42 -0700

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_numbers- Entry point
+* Return: void (Success)
+*/
+void print_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
