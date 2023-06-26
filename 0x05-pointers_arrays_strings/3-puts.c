1d36bc998875775f8d8c9274c1ded85bd1ddfba6
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-26 16:00:48 -0700

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,12 @@
+#include "main.h"
+/**
+*   _puts- Entry point
+* @str: pointer to pass to _puts function
+* Return: integer (Success)
+*/
 void _puts(char *str)
 {
-for (;str>0 && str >127 ;str++ )
+for (; str > 0 && str > 127; str++)
 {
 _putchar(*str);
 }
