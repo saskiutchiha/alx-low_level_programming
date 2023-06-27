2bfa7289fb30332e35932699c2b98f3b97fd934f
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-26 17:32:03 -0700

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,11 @@ void puts_half(char *str)
 int i;
 if (strlen(str) % 2 == 1)
 {
-i = (strlen(str) + 1)/2;
+i = (strlen(str) + 1) / 2;
 }
 else
 {
-i = strlen(str)/2;
+i = strlen(str) / 2;
 }
 str = str + i;
 for (; *str > 0 && *str < 127; str++)
