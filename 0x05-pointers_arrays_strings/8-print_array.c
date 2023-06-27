2910210f75026d499aaedf17552359db9c14df32
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-26 17:44:31 -0700

    Create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*   print_array- Entry point
+* @a: pointer to pass to _print_array function
+* @n: variable to pass to _print_array function
+* Return: void (Success)
+*/
+void print_array(int *a, int n)
+{
+if (n == 1)
+{
+_putchar(*a);
+}
+else
+{
+_putchar(*a);
+_putchar(',');
+_putchar(' ');
+return (print_array(a+1, n-1));
