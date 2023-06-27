62e633abf153f71f2f3aa0e667bffb6c77ad9606
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-06-26 18:56:44 -0700

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int _atoi(char *s)
-int i ,s = 0,a=0;
+int i ,sum = 0,a=0;
 {
 for (;*s > 0 && *s < 127; s++)
 {
@@ -9,14 +9,14 @@ if (*s == '-')
 a++;
 }
 i = *s - 48;
-s = s * 10 + i 
+s = sum * 10 + i 
 }
 if (a == 0)
 {
-printf("%d",s);
+printf("%d",sum);
 }
 else 
 {
-printf("%d",-s);
+printf("%d",-sum);
 }
 }
