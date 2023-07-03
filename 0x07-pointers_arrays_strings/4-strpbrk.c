9603d14d9f214553cca900037d00a36bb0ea1c46
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-03 16:22:12 -0700

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,17 @@
 #include <stddef.h>
+/**
+*  _strpbrk- Entry point
+* @accept: pointer to pass to _strpbrk function
+* @s: pointer to pass to _strpbrk function
+* Return: char (Success)
+*/
 char *_strpbrk(char *s, char *accept)
 {
 char *d;
 d = accept;
-for (;*s < 127 && *s >0;s++)
+for (; *s < 127 && *s > 0; s++)
 {
-for (;*accept < 127 && *accept > 0;accept++)
+for (; *accept < 127 && *accept > 0; accept++)
 {
 if (*s == *accept)
 {
