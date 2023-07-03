704219ebfd329740f9816efe47d6cf4acce81f24
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-03 16:15:20 -0700

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,10 @@
 #include <stddef.h>
+/**
+*  _strchr- Entry point
+* @s: pointer to pass to _strchr function
+* @c: pointer to pass to _strchr function
+* Return: char (Success)
+*/
 char *_strchr(char *s, char c)
 {
 for (;*s < 127 && *s > 0;s++)
