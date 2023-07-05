3b66ebfdeffbac20fbfb54df654672b2808a12c0
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-05 15:55:01 -0700

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,15 +1,20 @@
+/**
+* factorial- Entry point
+* @n: Number to pass to  factorial function
+* Return: integer (Success)
+*/
 int factorial(int n)
 {
 if (n < 0)
 {
-return -1;
+return (-1);
 }
 if (n == 0)
 {
-return 1;
+return (1);
 }
 else
 {
-return n * factorial(n-1);
+return (n * factorial(n-1));
 }
 }
