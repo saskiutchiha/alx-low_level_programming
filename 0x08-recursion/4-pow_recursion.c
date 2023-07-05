a625ba33df10ad700645c9cfa750ccc1fb4891fa
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-05 16:03:30 -0700

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,21 @@
+/**
+* _pow_recursion- Entry point
+* @x: Number to pass to  _pow_recursion function
+* @y: Number to pass to  _pow_recursion function
+* Return: integer (Success)
+*/
 int _pow_recursion(int x, int y)
 {
 if (y < 0)
 {
-  return -1;
+return (-1);
 }
 if (y == 0)
 {
-return 1;
+return (1);
 }
 else
 {
-return x * _pow_recursion(x, y-1);
+return (x * _pow_recursion(x, y - 1));
 }
 }
