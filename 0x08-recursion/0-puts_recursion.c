047889e8d75d6387da336327a6d31e5baae5ac42
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-05 15:45:21 -0700

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*    _puts_recursion Entry point
+* _puts_recursion Entry point
 * @s: Number to pass to  _puts_recursion function
 * Return: void (Success)
 */
@@ -9,7 +9,7 @@ void _puts_recursion(char *s)
 if (*s > 0 && *s < 127)
 {
 putchar(*s);
-_puts_recursion(s+1);
+_puts_recursion(s + 1);
 }
 else
 {
