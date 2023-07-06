8cc9695aa6117689f811b8f42d2f9c2bc8c3b51b
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-06 06:09:05 -0700

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+* _print_rev_recursion- Entry point
+* @s: Number to pass to  _print_rev_recursion function
+* Return: void (Success)
+*/
 void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
