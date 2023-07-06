ea4065d45aedfa407bf82c25069bae05fc430fac
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-06 06:00:28 -0700

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 void _print_rev_recursion(char *s) {
     if (*s == '\0') {
-        return;  // Base case: end of the string
+        return;  
     }
     
-    _print_rev_recursion(s + 1);  // Recursive call with the next character
+    _print_rev_recursion(s + 1);
     
-    printf("%c", *s);  // Print the current character
+    printf("%c", *s);  
 }
