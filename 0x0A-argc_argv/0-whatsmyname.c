10c24ee834f80a4ecc66f7f3adf92defe8dc5e59
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-08 08:09:14 -0700

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *@argc*: variable to pass to main function
- *@argv : variable to pass to main function
+ *@argc: variable to pass to main function
+ *@argv[]: variable to pass to main function
  * Return: Always 0 (Success)
  */
-int main(int argc, char* argv[])
+int main (int argc, char* argv[])
 {
 printf("%s\n", argv[argc - 1]);
 return (0);
