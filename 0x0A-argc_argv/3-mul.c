be5567a5a0f4063e713fbbce1ab80e6d79c0e569
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-08 16:32:07 -0700

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc , char* argv[])
+{
+long int sum1=0,i,j,sum2=0,x=0,y;
+if (argc == 1)
+{
+printf("Error");
+}
+else
+{
+for (i =1;i<3;i++)
+{
+y = strlen(argv[i]);
+for (j=0;j<y;j++)
+{
+if (argv[i][j]=='-')
+{
+x++;
+}
+else
+{
+if (i == 1)
+{
+sum1 = sum1*10 + argv[i][j] - 48;
+}
+else
+{
+sum2 = sum2*10 + argv[i][j] - 48;
+}
+}
+}
+}
+if (x==1)
+{
+printf("%ld\n",-sum1*sum2);
+}
+else
+{
+printf("%ld\n",sum1*sum2);
+}
+}
+return (0);
+}
+
+
+
+
+
+
