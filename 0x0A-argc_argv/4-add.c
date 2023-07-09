fae308b4ff6ea55aae96992c4788ba653562cd44
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-08 17:37:31 -0700

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc , char* argv[])
+{
+long int sum1=0,i,j,sum2=0,x=0,y;
+if (argc == 1)
+{
+printf("Error\n");
+}
+else
+{
+for (i =1;i<argc-1;i++)
+{
+sum1=0;
+y = strlen(argv[i]);
+for (j=0;j<y;j++)
+{
+if(argv[i][j] < 48 || argv[i][j] > 57)
+{
+z=1;
+printf("Error\n");
+break;
+}
+sum1 = sum1*10 + argv[i][j] - 48;
+}
+if (z==1)
+{
+break;
+}
+sum2 = sum2 + sum1;
+}
+if (z!=1)
+{
+printf("%ld\n",sum2);
+}
+}
+
+return (0);
+}
+
+
+
+
