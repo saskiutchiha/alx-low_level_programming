f358809bb2a29669f13cebf141b87d975cb6bbbb
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-08 18:44:04 -0700

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-int main(int argc , char* argv[])
+/**
+ * main - Entry point
+ *@argc : variable to pass to main function
+ *@argv : variable to pass to main function
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char* argv[])
 {
 int i;
-for (i=0;i<argc;i++)
+for (i=0; i < argc; i++)
 {
-printf("%s\n",argv[i]);
+printf("%s\n", argv[i]);
 }
 return (0);
 }
