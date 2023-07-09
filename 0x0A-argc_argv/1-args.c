057a3150df42446ede47a02cb83f81c4ab48d136
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-08 18:40:51 -0700

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *@argc : variable to pass to main function
+ *@argv : variable to pass to main function
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char* argv[])
 {
 if (argv[0][0] == 'n')
