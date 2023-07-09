af578dbe5aec4b4d0efda7763306c3ffe518e4d1
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-08 18:09:02 -0700

    Create 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,68 @@
+int main (int argc ; char* argv[])
+{
+if (argc !=2)
+{
+printf("Error");
+}
+else
+{
+if (argv[1][0]=='-')
+{
+printf("0\n");
+}
+else
+{
+y = strlen(argv[1]);
+for (j=0;j<y;j++)
+{
+ 
+sum1 = sum1*10 + argv[1][j] - 48;
+}
+while(sum1 != 0)
+{
+if (sum1 >= 25)
+{
+ sum1 = sum1 - 25 ;
+z++;
+}
+else
+{
+if (sum1 >= 10)
+{
+sum1 = sum1 - 10;
+z++;
+}
+else 
+{
+if (sum1 >= 5)
+{
+sum1 = sum1 - 5;
+z++;
+}
+else
+{
+if (sum1 >= 2)
+{
+sum1 = sum1 - 2;
+z++;
+}
+else
+{
+sum1--;
+z++;
+}
+}
+}
+}
+}
+}
+
+return (1);
+}
+
+
+
+
+
+
+
