0554ed95b90d75f6592de70dd4c0091dc2c847e3
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-12 15:02:45 -0700

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,19 +6,19 @@
  *@argv : variable to pass to main function
  * Return: Always 0 (Success)
  */
-int main (int argc , char* argv[])
+int main (int argc , char *argv[])
 {
-long int sum1=0,i,j,sum2=0,x=0,y;
+long int sum1 = 0, i, j, sum2 = 0, x = 0, y;
 if (argc == 1 || argc == 2)
 {
 printf("Error\n");
 }
 else
 {
-for (i =1;i<3;i++)
+for (i = 1; i < 3; i++)
 {
 y = strlen(argv[i]);
-for (j=0;j<y;j++)
+for (j = 0; j < y; j++)
 {
 if (argv[i][j]=='-')
 {
@@ -28,22 +28,22 @@ else
 {
 if (i == 1)
 {
-sum1 = sum1*10 + argv[i][j] - 48;
+sum1 = sum1 * 10 + argv[i][j] - 48;
 }
 else
 {
-sum2 = sum2*10 + argv[i][j] - 48;
+sum2 = sum2 * 10 + argv[i][j] - 48;
 }
 }
 }
 }
 if (x==1)
 {
-printf("%ld\n",-sum1*sum2);
+printf("%ld\n",-sum1 * sum2);
 }
 else
 {
-printf("%ld\n",sum1*sum2);
+printf("%ld\n",sum1 * sum2);
 }
 }
 return (0);
