f85b84ce7b8f3c5bd7eac68875b1bb133ad5b823
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-12 15:06:04 -0700

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,42 +6,40 @@
  *@argv : variable to pass to main function
  * Return: Always 0 (Success)
  */
-int main (int argc , char* argv[])
+int main (int argc, char *argv[])
 {
-long int sum1=0,i,j,sum2=0,z=0,y;
+long int sum1 = 0, i, j, sum2 = 0, z = 0, y;
 if (argc == 1)
 {
 printf("0\n");
 }
 else
 {
-for (i =1;i<argc;i++)
+for (i = 1; i < argc; i++)
 {
-        sum1=0;
+sum1 = 0;
 y = strlen(argv[i]);
-for (j=0;j<y;j++)
+for (j = 0; j < y; j++)
 {
- 
 if(argv[i][j] < 48 || argv[i][j] > 57)
 {
-z=1;
+z = 1;
 printf("Error\n");
 break;
 }
-sum1 = sum1*10 + argv[i][j] - 48;
+sum1 = sum1 * 10 + argv[i][j] - 48;
 }
-if (z==1)
+if (z == 1)
 {
 break;
 }
 sum2 = sum2 + sum1;
 }
-if (z!=1)
+if (z != 1)
 {
-printf("%ld\n",sum2);
+printf("%ld\n", sum2);
 }
 }
-
 return (0);
 }
 
