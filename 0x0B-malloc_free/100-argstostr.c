ba6441f3e490439f751eb6b6466add1e32ce76a2
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-11 17:50:45 -0700

    Create 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+char *argstostr(int ac, char **av);
+
+{
+char** q;
+int i,j,a=0,b;
+if (a== 0 || av == NULL)
+{
+return NULL;
+}
+for(i=0;i<ac;i++)
+{
+b = strlen(av[i]);
+a = a + b;
+{
+q = malloc(ac * sizeof(char*));
+if (q == NULL)
+{
+return NULL;
+}
+for (i=0;i<ac;i++)
+{
+b = strlen(av[i]);
+q[i] = malloc((b+1) * sizeof(char));
+}
+b = strlen(av[i]) ;
+for (i=0;i<ac;i++)
+{
+for (j=0;j < b;j++)
+{
+q[i][j] = av[i][j];
+}
+q[i][j] = '\n';
+}
+return q;
+}
