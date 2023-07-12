d728a5659eb7dbd277dc408f17916d2191d56e5e
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-11 18:51:44 -0700

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,23 +1,28 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
+/**
+ * _strdup- Entry point
+ *@str : variable to pass to _strdup function
+ * Return: char (Success)
+ */
 char *_strdup(char *str)
 {
 char* q;
-int i,d=strlen(str);
-  if (str == NULL)
-  {
-    q = NULL;
-    return (q);
-  }
-q = malloc(strlen(str)*sizeof(char));
+int i ,d = strlen(str);
+if (str == NULL)
+{
+q = NULL;
+return (q);
+}
+q = malloc(strlen(str) * sizeof(char));
 if (q == NULL)
 {
 return NULL;
 }
-for (i=0;i<d;i++)
+for (i = 0; i < d; i++)
 {
-*(q+i) = *(str+i);
+*(q + i) = *(str + i);
 }
 return q;
 }
