ca620a6a7c77f2f07e91945250af9fadd2ce1fba
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-11 18:03:02 -0700

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -4,7 +4,7 @@
 char *argstostr(int ac, char **av)
 
 {
-char** q;
+char* q;
 int i,j,a=0,b;
 if (a== 0 || av == NULL)
 {
@@ -13,9 +13,9 @@ return NULL;
 for(i=0;i<ac;i++)
 {
 b = strlen(av[i]);
-a = a + b;
+a = a + b+1;
 {
-q = malloc(ac * sizeof(char*));
+q = malloc(a * sizeof(char));
 if (q == NULL)
 {
 return NULL;
@@ -25,14 +25,18 @@ for (i=0;i<ac;i++)
 b = strlen(av[i]);
 q[i] = malloc((b+1) * sizeof(char));
 }
-b = strlen(av[i]) ;
+b=0;
 for (i=0;i<ac;i++)
 {
+  if (i != 0)
+  {
+  b = b+ strlen(av[i-1]) +1;
+  }
 for (j=0;j < b;j++)
 {
-q[i][j] = av[i][j];
+*(q+j+b) = av[i][j];
 }
-q[i][j] = '\n';
+*(q+j+b) = '\n';
 }
 return q;
 }
