22f880fd0cf605f3417ea36a0b5ca8e10d3bd44c
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-11 19:00:18 -0700

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,23 +1,28 @@
 #include <stdlib.h>
 #include <stddef.h>
+/**
+ * create_array- Entry point
+ *@c : variable to pass to create_array function
+ *@size : variable to pass to create_array function
+ * Return: char (Success)
+ */
 char *create_array(unsigned int size, char c)
-
 {
-char* q;
-int i,d;
-  d = size;
+char *q;
+int i, d;
+d = size;
 if (size == 0)
 {
-  return NULL;
+return (NULL);
 }
-q = malloc(size*sizeof(char));
-  if (q == NULL)
-  {
-return NULL;
-  }
-for (i=0;i<d;i++)
+q = malloc(size * sizeof(char));
+if (q == NULL)
 {
-*(q +i)= c;
+return (NULL);
+}
+for (i = 0; i < d; i++)
+{
+*(q + i) = c;
 }
 return (q);
 }
