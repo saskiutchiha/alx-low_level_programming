97bc35309e0094729f6a6b4034ddf488d95ec11a
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-14 08:04:43 -0700

    Create 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,31 @@
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+int a,i,b,c,d;
+char *q;
+
+b = strlen(s1);
+c= n;
+d = strlen(s2);
+a = strlen(s1) + c;
+if (c > d)
+{
+c = d;
+}
+q = malloc(a * sizeof(char));
+if (q == NULL)
+{
+return q;
+}
+for (i=0;i<a;i++)
+{
+if(i < b)
+{
+*(q+i) = *(s1+i);
+{
+else
+{
+*(q+i) = *(s2 +i-b)
+}
+}
+return q;
+}
