11c4b0891dde5db8cb2a3b7aa0c12e188eba5fb8
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-14 18:25:20 -0700

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,28 +1,32 @@
 #include <stdlib.h>
 #include <stddef.h>
-
-
+/**
+ * array_range- Entry point
+ *@min : variable to pass to array_range function
+ *@max : variable to pass to array_range function
+ * Return: char (Success)
+ */
 int *array_range(int min, int max)
 {
-int *q,i;
+int *q, i;
 if (min > max)
 {
-return NULL;
+return (NULL);
 }
 else
 {
-q = malloc((max-min+1) * sizeof(int));
+q = malloc((max-min + 1) * sizeof(int));
 if (q == NULL)
 {
-return NULL;
+return (NULL);
 }
 else
 {
-for (i =min;i<= max;i++)
+for (i = min; i <= max; i++)
 {
-*(q+i-min) = i;
+*(q + i - min) = i;
 }
 }
-return q;
+return (q);
 }
 }
