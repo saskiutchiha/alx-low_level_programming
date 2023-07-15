259ae633c57b857ad144fe5e646dde926815602b
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-14 18:45:30 -0700

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,9 @@
 #include <stdlib.h>
+/**
+ * create_array- Entry point
+ *@b : variable to pass to *malloc_checked function
+ * Return: void (Success)
+ */
 void *malloc_checked(unsigned int b)
 {
 if (b == 0)
