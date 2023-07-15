7f651d39901d2c99f39050870bdb80c90425d2b4
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-14 18:29:38 -0700

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,21 +1,27 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
+/**
+ * _calloc- Entry point
+ *@c : variable to pass to _calloc function
+ *@size : variable to pass to _calloc function
+ * Return: void (Success)
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *ptr;
 if (nmemb == 0 || size == 0)
 {
- return NULL;
+return (NULL);
 }
 else
 {
 ptr = malloc(nmemb * size);
 if (ptr == NULL)
 {
- return NULL;
+return (NULL);
 }
 memset(ptr, 0, nmemb * size);
-return ptr;
+return (ptr);
 }
 }
