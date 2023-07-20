822a58cb7452ca56b3cf543a46b62e707b060bc3
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-20 15:48:52 -0700

    Create 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,15 @@
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...)
+{
+int a = n,i,x;sum;
+va_list arg;
+va_start(arg, n);
+for (i=0;i<a;i++)
+{
+x = va_arg(arg,int);
+sum = sum + x;
+}
+va_end(arg);
+return sum;
+
+}
