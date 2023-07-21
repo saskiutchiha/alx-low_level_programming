8385a1b19abbd18a2cf0335fc519405bc87f0576
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-20 19:30:20 -0700

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,15 +1,19 @@
 #include <stdarg.h>
+/**
+ * sum_them_all - Entry point
+ *@n : variable to pass to sum_them_all function
+ * Return: integer (Success)
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-int a = n,i,x,sum=0;
+int a = n, i, x, sum = 0;
 va_list arg;
 va_start(arg, n);
-for (i=0;i<a;i++)
+for (i = 0; i < a; i++)
 {
-x = va_arg(arg,int);
+x = va_arg(arg, int);
 sum = sum + x;
 }
 va_end(arg);
-return sum;
-
+return (sum);
 }
