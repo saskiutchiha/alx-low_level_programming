95914547820770c477c2a2f263981885eef458db
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-20 19:22:13 -0700

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,39 +1,35 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void print_arguments(const char * const format, ...) {
-int i=0;
-char type;
+void print_all(const char * const format, ...) {
+
+    
+    int i;
+    float f;
+    char *s,c;
+
     va_list args;
     va_start(args, format);
 
-    while ( *(format + i) != '\0') {
-        type = *(formst+i);
-       switch (type) {
-            case 'i':
-                printf("%d ", va_arg(args, int));
-                break;
-            case 'f':
-                printf("%f ", va_arg(args,double));
-                break;
-            case 'c':
-                printf("%c ", va_arg(args, int)); 
-                break;
-            case 's':
-                
-    if (va_arg(args, char*) == NULL)
-{
-printf("(nil)");
-}
-if (va_arg(args, char*) != NULL)
-{
-printf("%s ", va_arg(args, char*));
-}
-                break;
-            default:
-                break;
-        }  
-        i++;
+    while (*format != '\0') {
+        if (*format == 'c') {
+            c = va_arg(args, int); // char is promoted to int when passed through ellipsis
+            printf("%c", c);
+        } else if (*format == 'i') {
+            i = va_arg(args, int);
+            printf("%d", i);
+        } else if (*format == 'f') {
+            f = va_arg(args, double); // float is promoted to double when passed through ellipsis
+            printf("%f", f);
+        } else if (*format == 's') {
+            s = va_arg(args, char*);
+            if (s != NULL) {
+                printf("%s", s);
+            } else {
+                printf("(nil)");
+            }
+        }
+        format++;
     }
 
     putchar('\n');
