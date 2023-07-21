37eed55120f6dbdefd31a7d719853e93a2e4bbd9
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-20 19:34:48 -0700

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,22 +1,24 @@
 #include <stdarg.h>
 #include <stdio.h>
+/**
+ *  print_numbers - Entry point
+ *@n : variable to pass to  print_numbersfunction
+ * Return: void (Success)
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-int a=n,i,x;
+int a = n, i, x;
 va_list arg;
-va_start(arg,n);
-for (i=0;i<a;i++)
+va_start(arg, n);
+for (i = 0; i < a; i++)
 {
-x = va_arg(arg,int);
-printf("%d",x);
+x = va_arg(arg, int);
+printf("%d", x);
 if (separator != NULL)
 {
-printf("%s",separator);
+printf("%s", separator);
 }
 }
 va_end(arg);
 putchar('\n');
 }
-
-
-
