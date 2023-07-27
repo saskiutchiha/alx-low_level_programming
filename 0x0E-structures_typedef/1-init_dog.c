51df106a100cce07befda681591a3180d9a4029c
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-26 20:11:37 -0700

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,10 @@
 #include <string.h>
 #include "dog.h"
 void init_dog(struct dog *d, char *name, float age, char *owner) {
+    if (d != NULL)
+    {
     d->name = strdup(name);
     d->owner = strdup(owner);
     d->age = age;
+    }
 }
