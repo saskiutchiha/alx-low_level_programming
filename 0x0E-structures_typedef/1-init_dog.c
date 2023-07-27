06f76701e5412bf95b83d0b75ed070ca278e6c9c
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-26 20:08:18 -0700

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
-#include <dog.h>
+#include "dog.h"
 void init_dog(struct dog *d, char *name, float age, char *owner) {
     d->name = strdup(name);
     d->owner = strdup(owner);
