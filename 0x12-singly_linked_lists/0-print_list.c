bf0818b697609c260c209130233df5dadd77b4f9
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-27 07:26:35 -0700

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+size_t print_list(const list_t *h)
+{
+list _t *q;
+size_t ;
+int a=0;
+while(q != NULL)
+{
+if (str == NULL)
+{
+printf("[0] (nil)\n");
+a++;
+}
+else
+{
+printf("[%d] (%s)\n",q->len,q->str);
+a++;
+}
+q = q->next;
+}
+n =a;
+return n;
+}
