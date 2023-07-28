d8309fb18cae00fdf2cdbe61e11b0ef6637cec26
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-27 17:38:50 -0700

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,15 +2,19 @@
 #include <stddef.h>
 #include "lists.h"
 #include <string.h>
-
+/**
+ * add_node_end - Entry point
+ *@head : variable to pass to add_node_end function
+  *@str : variable to pass to add_node_end function
+ * Return: list_t (Success)
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *q,*p =*head;
-
+list_t *q,*p = *head;
 q = malloc(sizeof(list_t));
-if (q ==NULL)
+if (q == NULL)
 {
-return NULL;
+return (NULL);
 }
 q->str = strdup(str);
 q->len = strlen(str);
@@ -21,11 +25,11 @@ if (*head == NULL)
 }
 else
 {
-while(p->next!=NULL)
+while(p->next != NULL)
 {
 p = p->next;
 }
 p->next = q;
 }
-return q;
+return (q);
 }
