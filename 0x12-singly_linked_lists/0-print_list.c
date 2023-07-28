4cbd81c76ceff4a435ef107d31424e97848a2fde
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-27 17:22:57 -0700

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,17 +1,21 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stddef.h>
+/**
+ * print_list - Entry point
+ *@h : variable to pass to print_list function
+ * Return: size_t (Success)
+ */
 size_t print_list(const list_t *h)
 {
 const list_t *q;
-
-size_t a=0;
-  q=h;
+size_t a = 0;
+q = h;
 if (q == NULL)
 {
-  return a;
+return (a);
 }
-while(q != NULL)
+while (q != NULL)
 {
 if (q->str == NULL)
 {
@@ -20,10 +24,10 @@ a++;
 }
 else
 {
-printf("[%u] %s\n",q->len,q->str);
+printf("[%u] %s\n", q->len, q->str);
 a++;
 }
 q = q->next;
 }
-return a;
+return (a);
 }
