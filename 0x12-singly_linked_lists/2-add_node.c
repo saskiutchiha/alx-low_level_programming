446226f9f8ef3f1107f154fa12d821a596f4f1ee
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-27 17:35:06 -0700

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,19 +2,24 @@
 #include <stddef.h>
 #include <string.h>
 #include "lists.h"
+/**
+ * add_node - Entry point
+ *@head : variable to pass to add_node function
+  *@str : variable to pass to add_node function
+ * Return: list_t (Success)
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *q;
-unsigned int a=strlen(str);
+unsigned int a = strlen(str);
 q = malloc(sizeof(list_t));
 if (q == NULL)
 {
-  return NULL;
+  return (NULL);
 }
 q->len = a;
 q->str = strdup(str);
 q->next = *head;
 *head = q;
-
-return q;
+return (q);
 }
