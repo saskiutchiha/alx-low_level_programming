25aee036fb04fca457ba42cc6e89e122164e2ac2
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-28 03:53:34 -0700

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,13 +13,13 @@ void print_all(const char * const format, ...) {
 
     while (*(format+i) != '\0') {
         if (*(format+i) == 'c') {
-            c = va_arg(args, int); // char is promoted to int when passed through ellipsis
+            c = va_arg(args, int); 
             printf("%c", c);
         } else if (*(format+i) == 'i') {
             i = va_arg(args, int);
             printf("%d", i);
         } else if (*(format+i) == 'f') {
-            f = va_arg(args, double); // float is promoted to double when passed through ellipsis
+            f = va_arg(args, double); 
             printf("%f", f);
         } else if (*(format+i) == 's') {
             s = va_arg(args, char*);
