ef733e56981fecb0ccb12e4b095cae900da6e690
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-28 03:28:02 -0700

    Create 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "dog.h"
+void print_dog(struct dog *d) {
+    if (d != NULL) {
+
+        if (d->name != NULL)
+            printf("Name: %s\n", d->name);
+        else
+            printf("Name: (nil)\n");
+
+ 
+        printf("Age: %d\n", d->age);
+
+ 
+        if (d->owner != NULL)
+            printf("owner: %s\n", d->owner);
+        else
+            printf("nil)\n");
+    }
+}
