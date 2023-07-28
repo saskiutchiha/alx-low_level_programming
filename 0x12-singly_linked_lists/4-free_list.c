32e4d956511eeb5a95bf148aecf7d44c55f6a71c
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-27 17:40:16 -0700

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,12 +1,16 @@
 #include <stdlib.h>
-#include "lists.h"
+#include "lists.h"/**
+ * free_list - Entry point
+ *@head : variable to pass to free_list function
+ * Return: void (Success)
+ */
 void free_list(list_t *head)
 {
-  list_t *q;
+list_t *q;
 if (head != NULL)
 {
-  q=head;
-  head = head->next;
+q = head;
+head = head->next;
 free(q->str);
 free(q);
 free_list(head);
