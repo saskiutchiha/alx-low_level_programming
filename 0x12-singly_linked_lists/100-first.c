0769b2e94be86544d5e249a726925f121ed01f06
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-27 17:49:22 -0700

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * pre_main - Entry point
+
+ * Return: void (Success)
+ */
 void __attribute__((constructor)) pre_main() 
 {
 printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
