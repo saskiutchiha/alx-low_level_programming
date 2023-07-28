84e9bdf8508fa03276f668316e41315b27444034
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-27 17:29:20 -0700

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,17 @@
 #include <stddef.h>
 #include "lists.h"
+/**
+ * list_len - Entry point
+ *@h : variable to pass to list_len function
+ * Return: size_t (Success)
+ */
 size_t list_len(const list_t *h)
 {
-size_t a=0;
+size_t a = 0;
 while(h != NULL)
 {
 a++;
 h = h->next;
 }
-return a;
+return (a);
 }
