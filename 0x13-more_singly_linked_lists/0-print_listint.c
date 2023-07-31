6255e1fc1d867d86dbfa7af27da8c740ba643670
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-07-31 05:56:41 -0700

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_listint - Entry point
+ *@h : variable to pass to print_listint function
+ * Return: size_t (Success)
+ */
+size_t print_listint(const listint_t *h)
+{
+const listint_t *q;
+size_t a = 0;
+q = h;
+if (q == NULL)
+{
+return (a);
+}
+while (q != NULL)
+{
+printf("%d\n",q->n)
+a++;
+q = q->next;
+}
+return (a);
+}
