99069f8c2563c6425470215e40e2730ef329122a
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-08-01 06:13:17 -0700

    Create 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *q,*p;
+q = head;
+while (q != NULL)
+{
+if (index == 0)
+{
+break;
+}
+q = q->next;
+index--;
+}
+if (q == NULL)
+{
+return NULL;
+}
+return q;
+}
