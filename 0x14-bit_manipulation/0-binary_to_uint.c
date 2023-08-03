d5c7808d450a03a267be887150898038fc7b0806
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-08-03 23:58:05 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,10 @@
 #include <string.h>
 #include <stddef.h>
-
+#include <math.h>
 unsigned int binary_to_uint(const char *b)
 {
 int a = strlen(b),i;
 unsigned int sum = 0;
-const char *q = b + a;
 i=a;
 if (b == NULL)
 {
@@ -13,11 +12,11 @@ return 0;
 }
 for (;i>=0;i--)
 {
-if (q[i] != '0' || q[i] != '1')
+if (b[i] != '0' && b[i] != '1')
 {
 return 0;
 }
-sum = sum + (q[i]+ 48) * (2 ** (a-i));
+sum = sum + (b[i] - 48) * pow(2,a-i);
 }
 return sum;
 }
