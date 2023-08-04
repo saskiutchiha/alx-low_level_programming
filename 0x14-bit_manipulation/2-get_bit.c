5cb656ab156c9f76f9de21435df6feff49655798
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-08-04 15:38:43 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@ int get_bit(unsigned long int n, unsigned int index){
  unsigned long int mask; 
     
      mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-     mask =   1 >> (63-index -1);
+     mask =   1 >> (64-index -1);
 if (index > 64)
 {
 return -1;
