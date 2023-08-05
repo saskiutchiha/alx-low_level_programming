b8ee2e06907fa454a49db56d80212c2f3797a963
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-08-05 04:00:30 +0100

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,10 +2,9 @@
 
 int get_endianness() {
     unsigned int num = 1;
-    // Create a pointer to the first byte of num
+    
     unsigned char *bytePtr = (unsigned char *)&num;
     
-    // If the first byte (lowest address) is 1, it's little endian
     if (*bytePtr == 1) {
         return 1; // Little endian
     } else {
