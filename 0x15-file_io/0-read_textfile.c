f17019eac597271ed399c9142510af1113bca4e5
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-08-08 01:25:14 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <stddef.h>
 ssize_t read_textfile(const char *filename, size_t letters) {
     if (filename == NULL) {
         return 0; 
