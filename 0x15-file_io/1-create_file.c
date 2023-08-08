cd22009f38f0b7ef9cbdc6a5448ac7caf13f788c
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-08-08 02:19:48 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int create_file(const char *filename, char *text_content)
 {
