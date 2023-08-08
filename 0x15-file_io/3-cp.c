b3229640374f9048258832b5cf38fb5344e3a543
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-08-08 02:41:06 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,28 +16,33 @@ void print_error(const char *message, const char *filename) {
 }
 
 int main(int argc, char *argv[]) {
+    const char *file_from;
+     const char *file_to;
+    int fd_from ;
+    int fd_to;
+      char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
     if (argc != 3) {
         print_usage();
         return 97;
     }
 
-    const char *file_from = argv[1];
-    const char *file_to = argv[2];
+    file_from = argv[1];
+    file_to = argv[2];
 
-    int fd_from = open(file_from, O_RDONLY);
+    fd_from = open(file_from, O_RDONLY);
     if (fd_from == -1) {
         print_error("Can't read from file", file_from);
         return 98;
     }
 
-    int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP);
+     fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP);
     if (fd_to == -1) {
         print_error("Can't write to", file_to);
         return 99;
     }
 
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_read;
+  
 
     while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0) {
         ssize_t bytes_written = write(fd_to, buffer, bytes_read);
