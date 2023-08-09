c555111c1d89befa0873a6adebe1fd5a23c7d818
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-08-09 01:56:19 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 
-int create_file(const char *filename, char *text_content)
-{
-FILE *f;
-struct stat st;
-f = fopen(filename ,"W");
-if (filename == NULL)
-{
-return 0;
-}
-   
-fwrite(text_content,1,strlen(text_content), f);
- fclose(f);
-    if (stat(filename, &st) == 0 && (st.st_mode & S_IRUSR) == 0) {
-        chmod(filename, S_IRUSR | S_IWUSR);
+int create_file(const char *filename, char *text_content) {
+    int fd ;
+   ssize_t text_length ;
+   ssize_t bytes_written ;
+    if (filename == NULL) {
+        return -1;
     }
 
-return 1;
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        return -1; 
+    }
+
+    if (text_content != NULL) {
+        text_length = strlen(text_content);
+         bytes_written = write(fd, text_content, text_length);
+        if (bytes_written != text_length) {
+            close(fd);
+            return -1; 
+        }
+    }
+
+    close(fd);
+
+    return 1; 
 }
