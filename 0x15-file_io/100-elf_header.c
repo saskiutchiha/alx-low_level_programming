7b67b0cc7d2ad2dcc776c4aa21aad959c224dd7b
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-08-09 02:52:45 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,16 +5,27 @@
 #include <elf.h>
 #include <string.h>
 
-void print_error(const char *message) {
+/**
+ * print_error - Print error message and exit with status code 98
+ * @message: The error message to be printed
+ */
+void print_error(const char *message)
+{
     fprintf(stderr, "Error: %s\n", message);
     exit(98);
 }
 
-void display_elf_header(const Elf64_Ehdr *header) {
+/**
+ * display_elf_header - Display information from ELF header
+ * @header: Pointer to the ELF header structure
+ */
+void display_elf_header(const Elf64_Ehdr *header)
+{
+    int i;
+
     printf("Magic: ");
-    for (int i = 0; i < EI_NIDENT; i++) {
+    for (i = 0; i < EI_NIDENT; i++)
         printf("%02x ", header->e_ident[i]);
-    }
     printf("\nClass: %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
     printf("Data: %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little-endian" : "2's complement, big-endian");
     printf("Version: %d\n", header->e_ident[EI_VERSION]);
@@ -24,25 +35,36 @@ void display_elf_header(const Elf64_Ehdr *header) {
     printf("Entry point address: 0x%lx\n", header->e_entry);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        print_error("Usage: elf_header elf_filename");
-    }
-
-    const char *filename = argv[1];
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        print_error("Unable to open file");
-    }
-
+/**
+ * main - Entry point of the program
+ * @argc: Argument count
+ * @argv: Array of arguments
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char *argv[])
+{
+    int fd;
+    ssize_t bytes_read;
     Elf64_Ehdr header;
-    ssize_t bytes_read = read(fd, &header, sizeof(header));
-    if (bytes_read != sizeof(header)) {
+    const char *filename;
+
+    if (argc != 2)
+        print_error("Usage: elf_header elf_filename");
+
+    filename = argv[1];
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        print_error("Unable to open file");
+
+    bytes_read = read(fd, &header, sizeof(header));
+    if (bytes_read != sizeof(header))
+    {
         close(fd);
         print_error("Error reading ELF header");
     }
 
-    if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0) {
+    if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
+    {
         close(fd);
         print_error("Not an ELF file");
     }
