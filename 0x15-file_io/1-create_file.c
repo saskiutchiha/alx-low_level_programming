6a5825228dff119a8fcc3d217b9d02ff74dd05fd
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-08-09 03:20:09 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,29 +9,15 @@
  *@filename : variable to pass to  create_file function
  * Return: int (Success)
  */
+
 int create_file(const char *filename, char *text_content) {
-    int fd ;
-   ssize_t text_length ;
-   ssize_t bytes_written ;
-    if (filename == NULL) {
-        return -1;
+    FILE *f;
+    f = fopen(filename, "w"); 
+    if (f == NULL) {
+        return 0;
     }
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1) {
-        return -1; 
-    }
-
-    if (text_content != NULL) {
-        text_length = strlen(text_content);
-         bytes_written = write(fd, text_content, text_length);
-        if (bytes_written != text_length) {
-            close(fd);
-            return -1; 
-        }
-    }
-
-    close(fd);
-
-    return 1; 
+    fwrite(text_content, 1, strlen(text_content), f);
+    fclose(f);
+    return 1;
 }
