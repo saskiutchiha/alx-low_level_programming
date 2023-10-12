b3b61407d6cb516e3f01700281edfaa06c27c785
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-10-12 19:55:00 +0100

    Create 0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+size_t print_dlistint(const dlistint_t *h){
+size_t a=0;
+while(h != NULL){
+printf("%d\nh->n);
+a++;
+h = h->next
+}
+return (a);
+}
