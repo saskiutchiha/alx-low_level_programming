ebfdd1c019e93c04f5e14b07d55520a95ebfa8c6
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2023-10-12 20:40:15 +0100

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stddef.h>
 #include "lists.h"
-size_t print_dlistint(const dlistint_t *h){
+size_t dlistint_len(const dlistint_t *h){
 size_t a=0;
 while(h != NULL){
 a++;
