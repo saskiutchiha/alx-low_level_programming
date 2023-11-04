8a05aa5d3130d3c880ab39768df76f499762ae8f
saskiutchiha <boulidamabdellah8@gmail.com>
2023-11-04 01:41:16 +0100

    ll

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	  hash_node_t *q = malloc(sizeof(hash_node_t));
+	  if (key == NULL)
+	  {
+		  return 0;
+	  }
+	  q-> key = strdup(key);
+	  q->value = strdup(value);
+	long unsigned int c =  hash_djb2(key) % ht->size ;
+	q->next = ht->array[c];
+	ht->array[c] = q;
+	return 1 ;
+	
+}
