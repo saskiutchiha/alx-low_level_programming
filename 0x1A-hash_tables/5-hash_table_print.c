c88d3c91d1315693f7417849b7e800414017a1ed
saskiutchiha <boulidamabdellah8@gmail.com>
2023-11-06 01:41:14 +0100

    ll

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdbool.h>
+void hash_table_print(const hash_table_t *ht)
+	
+{ 
+	bool check;
+	hash_node_t **p;
+        unsigned long int i ;	
+	check = false;
+	if (ht != NULL)
+	{
+		printf("{");
+		for(i =0 ;i < ht->size;i++)
+		{
+			p = ht->array + i;
+			while(*p != NULL)
+			{
+			 
+			  if (check == true)
+			  {
+				  printf(",");
+			  }
+                         printf("%s: %s",(*p)->key,(*p)->value);
+			 check = true ;
+			 p = &((*p)->next);
+
+			}
+		}
+		printf("}\n");
+	}
+}
