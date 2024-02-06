863d8ca98614cfa1822a9948523d110029bd632a
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-02-06 15:34:48 +0100

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -4,8 +4,9 @@
  * print_array - prints an array of integers
  * @array: pointer to the start of the array
  * @size: size of the array (number of elements)
+ * @value: value to searched for
  *
- * Return: nothing
+ * Return: 1st index where value is located or -1 if not found or array is null
  */
 int binary_search(int *array, size_t size, int value){
 	int *p = array + size-1,mil,index,*q = array,i;
