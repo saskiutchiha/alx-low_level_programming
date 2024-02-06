a98bd2d9ac2af0440a33e952e273bc891ce65c55
saskiutchiha <boulidamabdellah8@gmail.com>
2024-02-06 14:23:12 +0100

    ll

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,11 +2,13 @@
 #include <stddef.h>
 int linear_search(int *array, size_t size, int value){
 	size_t i ;
-	int ret ;
+	int ret ,j;
 	if (array == NULL){
             return -1;
 	}
 	for(i = 0;i<size;i++){
+		j = i;
+		printf("Value checked array[%d] = [%d]\n",j,*(array+i));
 		if (*(array + i) == value){
 			ret = i;
 			return ret;
